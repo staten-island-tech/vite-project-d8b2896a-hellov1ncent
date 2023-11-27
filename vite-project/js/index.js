21fdce4a17c332c2d803e66e0e3c36eb933834ae
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-11-27 10:13:53 -0500

    ew

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -6,4 +6,10 @@ document.querySelector(".btn").addEventListener("click", function (){
         document.body.classList.add("cold");
         document.body.classList.remove("hot");
     }
+<<<<<<< HEAD
 });
+=======
+});
+
+import "../styles/style.css"
+>>>>>>> parent of c8b2e5c (sd)
