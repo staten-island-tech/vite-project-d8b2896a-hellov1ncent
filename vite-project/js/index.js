d7a6a463d6eb12bd70ef4c3c87e651ba7d20f642
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-11-28 10:11:16 -0500

    aaa

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -9,3 +9,49 @@ document.querySelector(".btn").addEventListener("click", function (){
 });
 
 import "../styles/style.css"
+
+
+
+
+const DOMSelectors = {
+    button: document.querySelectorAll(".btn"),
+    lemonadebtn: document.querySelector('.Original'),
+    fruitbtn: document.querySelector('.fruit'),
+    lemonadebtn: document.querySelector('.SugarFree'),
+    hotbtn: document.querySelector('.hot'),
+    coldbtn:document.querySelector('.cold')
+}
+
+
+function clearfields(){
+    DOMSelectors.column.innerHTML="";
+}
+
+
+function insertCards(arr){
+    arr.array.forEach(lemonade => {
+        DOMSelectors.column.insertAdjacentHTML(
+            "beforeend",
+            `<div class="card">
+                <h3 class = "name">${cards.name}</h3>
+                <img src="${cards.img}" class="img">
+                <h4>Price: ${cards.price}</h4>
+            </div>`
+        )
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
